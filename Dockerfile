63dc659693dccbd25a6ebd434218fb7cf9d93231
marques-j-robinson <marques.j.robinson@gmail.com>
2022-09-18 10:51:41 -0700

    init

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.8-slim-buster
+
+WORKDIR /app
+
+# We copy just the requirements.txt first to leverage Docker cache
+COPY ./requirements.txt /app/requirements.txt
+
+RUN pip install -r requirements.txt
+
+COPY . /app
+
+ENTRYPOINT [ "python" ]
+
+CMD [ "app.py" ]
---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+app.debug = True
+
+
+all_locations = {
+    'new-york': {
+        'temp': -5,
+        'spice': 'Hell hath froze over',
+    },
+    'seattle': {
+        'temp': 65,
+        'spice': "I'd build a summer home here",
+    },
+    'san-antono': {
+        'temp': 200,
+        'spice': "It's hot as balls",
+    },
+}
+
+
+@app.route('/')
+def index():
+    return 'Welcome to the Spicy Weather API'
+
+
+@app.route('/locations')
+def locations():
+    print(locations)
+    return jsonify(data=list(all_locations.keys()))
+
+
+@app.route('/weather/<city>')
+def weather(city):
+    try:
+        return jsonify(data=all_locations[city])
+    except KeyError:
+        return jsonify(data='city_not_found')
+    except:
+        return jsonify(data='internal_server_error')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
